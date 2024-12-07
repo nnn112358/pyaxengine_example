b3b94759b058836ae252d71249093a3560e5009f
nnn <27625496+nnn112358@users.noreply.github.com>
2024-12-08 00:49:38 +0900

    Update classification_opencv.py

--- classification_opencv.py
+++ classification_opencv.py
@@ -1,9 +1,4 @@
-# Copyright (c) 2019-2024 Axera Semiconductor Co., Ltd. All Rights Reserved.
-#
-# This source file is the property of Axera Semiconductor Co., Ltd. and
-# may not be copied or distributed in any isomorphic form without the prior
-# written consent of Axera Semiconductor Co., Ltd.
-#
+# Copyright (c) 2024 nnn112358
 
 import axengine as axe
 import numpy as np
